e7381449439375467bc39c7ecefa293b931d913e
Kitainix <dmtopskiy@gmail.com>
2023-12-25 21:10:23 +0500

    Добавьте файлы проекта.

---
+++ Main.cpp
@@ -0,0 +1,50 @@
+﻿#include <iostream>
+#include "Functions.h"
+#include "Structures.hpp"
+#include "Data.hpp"
+
+
+int main() {
+	setlocale(LC_ALL, "Russian");
+	int n;
+	
+
+	// Файл с функциями.
+	/*
+	std::cout << sum(5, 4) << std::endl;
+	prime_range(7, 20);
+
+	const int size = 3;
+	int arr[size]{ 10, 20, 30 };
+	print_arr(arr, size);
+	*/
+	
+
+	// Файл со структурами.
+	/*
+	Person p1{
+		"Lara Croft",
+		{ 20, 5, 1995 },
+		'F'
+	};
+	print_person(p1);
+	*/
+
+
+	// Файл с переменными.
+	_A = 7;
+	std::cout << "A = " << _A << std::endl;
+	std::cout << "Pi = " << _PI << std::endl;
+	print_arr(_ARR, _SIZE);
+	prime_range(1, 10);
+	
+
+
+	return 0;
+}
+
+// Области применения многофайловых проектов:
+// 1. Сокращение объёма кода в главном файле;
+// 2. Разделение обязанностей разработчиков;
+// 3. Создание своих библиотек;
+// 4. Ускорение сборки;
---
+++ Structures.cpp
@@ -0,0 +1,7 @@
+﻿#include "Structures.hpp"
+
+void print_person(const Person& P) {
+	std::cout << "Имя: " << P.name << std::endl;
+	std::cout << "Дата рождения: " << P.birthday.day << '.' << P.birthday.month << '.' << P.birthday.year << std::endl;
+	std::cout << "Пол: " << (P.gender == 'M' ? "Мужской" : "Женский") << std::endl;
+}
